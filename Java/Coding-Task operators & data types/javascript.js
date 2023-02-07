38199415be59f8b3afeb2e98be7e74ace847b9f6
Arne KLussmann <ProKlussi@gmail.com>
2023-02-07 21:54:32 +0100

    tSwitchIF

---
+++ Java/Coding-Task operators & data types/javascript.js
@@ -0,0 +1,45 @@
+console.log("HEllo Arne Yeah"); 
+
+console.log("A"+"r"+"n"+"e")
+
+let Vorname = "Arne";
+let Nachname ="Klußmann"
+
+console.log(Vorname +" "+ Nachname);
+
+const Person = { "Vorname" : "Arne",
+                "Nachname": "Klußmann",
+                "Alter" : 35,               }
+
+console.log(Person);
+
+
+const Girls = [];
+Girls.push( "Gay","Eva","Lissy","Nathalie","Pia","Christin","Pia",
+            "Patricia","Lea","Charline","Alena","Arzu","Neele","Swanni",
+            "Johanna","Britta","Kristin","Chrstin","Merle",
+            "Maike","Stadthagen","Köln","Kreml","Hochzeit Theresa","Wichmann+Freimarkt",
+            "Rimini","Arzu","Stubu","Anja","Jasmin","Elena","Gesa","Olga","Silja","Pia","Mareike",
+            "Bulgarien","Yabu","Besuch in Dortmund",
+            )
+
+console.log(Girls.length);
+
+let anzahl = Girls.length;
+
+for ( let i = 0 ; i < anzahl; i++) { 
+console.log(Girls[i]);}
+
+
+
+console.log(50 + 2 ===  52);
+
+console.log("333" === 333);
+
+console.log((600 + 510) / 2);
+console.log(2 + 2);
+
+
+
+console.log( 5 === (25 / 5)); // Result: 5
+console.log(50 / 10); // Result: 5
