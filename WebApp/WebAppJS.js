02f4edda2a5aaaedd47384af3e23c9fe1f845ac0
Arne KLussmann <ProKlussi@gmail.com>
2023-03-29 21:49:04 +0200

    HintergrundButton

--- WebApp/WebAppJS.js
+++ WebApp/WebAppJS.js
@@ -18,3 +18,36 @@ function Datum() {
   var ausgabeD = datum;
   document.getElementById("Datum").value = ausgabeD;
 }
+
+function hintergrund() {
+
+  var header = document.getElementsByTagName("header")[0];
+        header.classList.toggle("darkmode");
+
+        var all = document.getElementsByTagName("html")[0];
+        all.classList.toggle("darkmode");
+   
+        var main = document.getElementsByTagName("main")[0];
+        main.classList.toggle("darkmode");
+
+        var hh = document.querySelectorAll("h2")[0];
+        hh.classList.toggle("darkmode");
+
+        var hhh = document.querySelectorAll("h3")[0];
+        hhh.classList.toggle("darkmode");
+
+
+        	// Definiere ein Array mit möglichen Hintergrundfarben
+			var colors = ['#FF5733', '#C70039', '#900C3F', '#581845', '#4CAF50', '#2196F3', '#9C27B0', '#F44336'];
+
+			// Wähle eine zufällige Farbe aus dem Array aus
+			var randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+			// Ändere die Hintergrundfarbe des Footer-Elements
+			var footer = document.getElementsByTagName("footer")[0];
+			footer.style.backgroundColor = randomColor;
+      }
+ 
+  
+
+
