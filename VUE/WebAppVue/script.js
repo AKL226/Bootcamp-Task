bb3582e01a79fb0ee5f019e7c28bb95644fbc82c
Arne KLussmann <ProKlussi@gmail.com>
2023-11-21 20:48:53 +0100

    update Kata

--- VUE/WebAppVue/script.js
+++ VUE/WebAppVue/script.js
@@ -3,11 +3,11 @@
     data(){ return {
 
 
-      checked: false,
-      prio: false,
+   
       taskNameN: '',
       taskDescN: '',
-
+      checked: false,
+      priori: false,
       tasksN: JSON.parse(localStorage.getItem('tasksN')) || []
     };
   },
@@ -16,52 +16,44 @@
 
 
     methods: {
-      renderTasksN() {
-        const taskListN = document.querySelector('#taskListN');
-        taskListN.innerHTML = '';
-  
-        this.tasksN.forEach((task, index) => {
-          const listItem = document.createElement('li');
-          listItem.innerHTML = `<input type="checkbox" class="check" v-model="tasksN[${index}].checked">
-                                <strong>${task.name}</strong>${task.desc}
-                                <button class="deleteBtnN" @click="deleteItemN(${index})">X</button>`;
-          taskListN.appendChild(listItem);
-        });
-        
-      },
+
+      focusNextField( nextField) {    
+          // Verweisen Sie auf das nächste Eingabefeld
+      
+          const nextFieldZiel = this.$refs[nextField];
+    
+          // Setzen Sie den Fokus auf das nächste Eingabefeld
+       
+            console.log(nextFieldZiel);
+            nextFieldZiel.focus();
+       
+    },
+
+
+
+
       addTaskN() {
-        if (!this.taskNameN.trim()) {
-          return;
-        }
-  
-        this.tasksN.push({ name: this.taskNameN, desc: this.taskDescN, checked: false , prio: false });
+        this.tasksN.push({ name: this.taskNameN, desc: this.taskDescN, checked: false , priori: false });
         localStorage.setItem('tasksN', JSON.stringify(this.tasksN));
-  
-        this.taskNameN = '';
-        this.taskDescN = '';
-  
       },
 
 
 
       deleteItemN(index) {
         this.tasksN.splice(index, 1);
-        localStorage.setItem('tasksN', JSON.stringify(this.tasksN));
-     
+        localStorage.setItem('tasksN', JSON.stringify(this.tasksN));   
       },
 
       
       checkCheck(index) {
         this.tasksN[index].checked = !this.tasksN[index].checked;
-        localStorage.setItem("tasksN", JSON.stringify(this.tasksN));
-      
+        localStorage.setItem("tasksN", JSON.stringify(this.tasksN));   
       },
 
 
-      prio(index) {
-        this.tasksN[index].prio = !this.tasksN[index].prio;
+      prioMethode(index) {
+        this.tasksN[index].priori = !this.tasksN[index].priori;
         localStorage.setItem("tasksN", JSON.stringify(this.tasksN));
-      
       },
 
       clearListee() {
@@ -69,11 +61,14 @@
         checkedItems.forEach(task => {
           const index = this.tasksN.indexOf(task);
           this.tasksN.splice(index, 1);
+
+          localStorage.setItem("tasksN", JSON.stringify(this.tasksN));
         });
   
-        localStorage.setItem("tasksN", JSON.stringify(this.tasksN));
+       
       
       }
     },
  
+      
   }).mount("#app");
