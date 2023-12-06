20a8d2ec29f418415a63ab4e4b262cbf30586468
Arne KLussmann <ProKlussi@gmail.com>
2023-12-06 22:13:51 +0100

    CodingTaskBookButton

---
+++ VUE/myfirstprojekt/src/components/PrioBook.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button @click="toggleState1">{{ buttonBuy }}</button>
+    <button @click="toggleState2">{{ buttonRead }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      booked: false,
+      read: false,
+    };
+  },
+  computed: {
+    buttonBuy() {
+      return this.booked ? "🔴" : "🟢";
+    },
+    buttonRead() {
+      return this.read ? "📖" : "📙";
+    },
+  },
+  methods: {
+    toggleState1() {
+      this.booked = !this.booked;
+    },
+    toggleState2() {
+      this.read = !this.read;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Füge hier deinen eigenen CSS-Stil hinzu, falls benötigt */
+</style>
